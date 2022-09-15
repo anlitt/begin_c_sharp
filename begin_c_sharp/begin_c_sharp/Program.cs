07ced46f1a6ee064b5488bc3f010c754ea2d20c5
Litt,Andreas <andreas.litt@tecalliance.net>
2022-09-15 09:13:13 +0200

    no message

--- begin_c_sharp/begin_c_sharp/Program.cs
+++ begin_c_sharp/begin_c_sharp/Program.cs
@@ -10,8 +10,8 @@ namespace begin_c_sharp
     {
         static void Main(string[] args)
         {
-            string userName = "Andreas";
-            Console.WriteLine("hello " + userName);
+            string userName123 = "Andreas";
+            Console.WriteLine("hello " + userName123);
             Console.ReadLine();
         }
     }
